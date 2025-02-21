aec03da250ea1b98e15e6cb9e01397af5be1c037
Ramin Abdulaev <rmnabdulaev@gmail.com>
2025-02-21 16:09:05 +0400

    worker projects

--- src/app/system-pages/worker/shared/models/worker-children-routes.ts
+++ src/app/system-pages/worker/shared/models/worker-children-routes.ts
@@ -1,8 +1,5 @@
 import { Route } from '@angular/router';
-import { ProjectDetailsComponent } from '../../../../pages/organization-projects/shared/pages/project-info/shared/pages/project-details/project-details.component';
-import { ProjectCalendarComponent } from '../../../../pages/organization-projects/shared/pages/project-info/shared/pages/project-calendar/project-calendar.component';
 import { WorkerProjectsComponent } from '../../../../pages/worker-projects/worker-projects.component';
-import { WorkerProjectInfoComponent } from '../../../../pages/worker-projects/shared/pages/worker-project-info/worker-project-info.component';
 import { InvitationsComponent } from '../../../../pages/invitations/invitations.component';
 import { WorkerProjectDetailsComponent } from '../../../../pages/worker-projects/shared/pages/worker-project-details/worker-project-details.component';
 
@@ -23,29 +20,6 @@ export class WorkerChildrenRoutes {
       component: WorkerProjectDetailsComponent,
       data: { title: 'Projects' },
     },
-    // {
-    //   path: 'projects/:id',
-    //   component: WorkerProjectInfoComponent,
-    //   data: { title: 'Projects' },
-    //   children: [
-    //     {
-    //       path: 'about',
-    //       component: ProjectDetailsComponent,
-    //       data: { title: 'Projects' },
-    //     },
-    //     {
-    //       path: 'calendar',
-    //       component: ProjectCalendarComponent,
-    //       data: { title: 'Projects' },
-    //     },
-    //     {
-    //       path: 'calendar/:id',
-    //       component: ProjectCalendarComponent,
-    //       data: { title: 'Projects' },
-    //     },
-    //     { path: '', redirectTo: 'about', pathMatch: 'full' },
-    //   ],
-    // },
     { path: '', redirectTo: 'projects', pathMatch: 'full' },
     { path: '**', redirectTo: 'projects', pathMatch: 'full' },
   ];
