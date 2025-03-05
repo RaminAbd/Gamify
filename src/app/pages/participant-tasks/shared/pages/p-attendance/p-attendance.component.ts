9d0b9f344e52352c512d81f8bc8e0be6be8fde99
RaminAbd <rmnabdulaev@gmail.com>
2025-03-05 11:42:07 +0400

    budgets

--- src/app/pages/participant-tasks/shared/pages/p-attendance/p-attendance.component.ts
+++ src/app/pages/participant-tasks/shared/pages/p-attendance/p-attendance.component.ts
@@ -2,13 +2,10 @@ import {Component, inject} from '@angular/core';
 import {AttendanceResponseModel} from '../../models/attendance-response.model';
 import {PAttendanceService} from './p-attendance.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-p-attendance',
-  imports: [
-    NgIf
-  ],
+  imports: [],
   templateUrl: './p-attendance.component.html',
   styleUrl: './p-attendance.component.scss'
 })
